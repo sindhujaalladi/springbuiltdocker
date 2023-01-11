aa261eb7d40b6dd64124e91f440cd3ea6003fc72
sindhujaalladi <sindhu.alladi1993@gmail.com>
2023-01-10 21:07:38 -0600

    Initial commit

---
+++ src/main/java/com/springbuiltdocker/springbuiltdocker/SpringbuiltdockerApplication.java
@@ -0,0 +1,13 @@
+package com.springbuiltdocker.springbuiltdocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbuiltdockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbuiltdockerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springbuiltdocker/springbuiltdocker/SpringbuiltdockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbuiltdocker.springbuiltdocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbuiltdockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
