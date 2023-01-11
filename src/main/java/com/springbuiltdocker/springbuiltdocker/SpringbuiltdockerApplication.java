0dd18d88f143ed1918142134aea4fcca0a8770cd
sindhujaalladi <sindhu.alladi1993@gmail.com>
2023-01-10 21:08:34 -0600

    inital commit

--- src/main/java/com/springbuiltdocker/springbuiltdocker/SpringbuiltdockerApplication.java
+++ src/main/java/com/springbuiltdocker/springbuiltdocker/SpringbuiltdockerApplication.java
@@ -2,12 +2,20 @@ package com.springbuiltdocker.springbuiltdocker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringbuiltdockerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringbuiltdockerApplication.class, args);
-	}
+    @GetMapping("/image")
+    public String runImage() {
+        return "docker image created and running successfully";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbuiltdockerApplication.class, args);
+    }
 
 }
--- src/test/java/com/springbuiltdocker/springbuiltdocker/SpringbuiltdockerApplicationTests.java
+++ src/test/java/com/springbuiltdocker/springbuiltdocker/SpringbuiltdockerApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SpringbuiltdockerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
